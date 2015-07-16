eb43c75c5b5c2a55d5dbdd322bbdd63e1e2e84f4
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2015-07-15 23:28:57 -0300

    inclusao das deps APOC e REDIS, criação da task de minify no gulp, criação da estrutura basica de views

--- back/gulpfile.js
+++ back/gulpfile.js
@@ -2,3 +2,19 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
+
+var jsFiles = ['/js/**/*.*'];
+
+gulp.task('minify',function(){
+  gulp.src(jsFiles)
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('sass',function(){
+  //TODO implementar funcionalidade com gulp-sass
+});
+
+gulp.task('default',['minify']);
